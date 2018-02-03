5ea64fc11a51a385b0074b9d825df4985893026c
dannyrc100 <dannyrc100@hotmail.com>
2018-02-03 01:41:52 -0500

    Setup word.js and game.js

---
+++ word.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var game = require('./game');
+
+
+function main_menu(){
+	var word;
+	var array = [];
+	var tries = 3;
+    fs.readFile("words.txt", "utf8", function(error, data) {
+		var dataArr = data.split(", ");
+		word = dataArr[Math.floor(Math.random()*dataArr.length)];
+		for (var i = 0; i < word.length; i++) {
+			if(word.charAt(i) === ' '){
+				array[i] = ' ';
+			}
+			else{
+		 	array[i] = "_ ";
+		 	}
+		}
+		var start = new game(word, array, tries, main_menu);
+		start.gameStart();
+    });
+}
+module.exports = main_menu;
+main_menu();
